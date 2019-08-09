ad5cc2a8e8d9aa4501a3d0837cec70a11850ef2b
MADC2018 <madousdebesc@gmail.com>
2019-08-09 13:08:34 -0500

    actualizacion2

--- controllers/estado_recibo.go
+++ controllers/estado_recibo.go
@@ -12,7 +12,7 @@ import (
 )
 
 // EstadoReciboController operations for EstadoRecibo
-// a ver si sube esta genialida
+// a ver si sube esta genialidaD
 
 type EstadoReciboController struct {
 	beego.Controller
